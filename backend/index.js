3ed9973a43ee8de60f5b35f2d54bbf98e6c81d18
AkkuAku <akku.saniyazbekova@nu.edu.kz>
2025-03-18 03:05:59 +0600

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ const { Pool } = require("pg");
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
 
 app.use(cors());
 app.use(express.json());
