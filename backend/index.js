21d6fe8af6b4289f0bfbf6ae33912354bf0af171
AkkuAku <akku.saniyazbekova@nu.edu.kz>
2025-07-25 13:04:41 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,9 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 
 // PostgreSQL Connection
 const pool = new Pool({
